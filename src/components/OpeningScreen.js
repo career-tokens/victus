abcb2a8c38e5f1c33462b732465f90a2a47ff7fa
mainak <heellomeelo@gmail.com>
2023-07-03 22:00:19 +0530

    opening screen and login page made

--- src/components/OpeningScreen.js
+++ src/components/OpeningScreen.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import "./OpeningScreen.css";
 import logo from "../victus_logo.jpg";
+import { useNavigate } from "react-router-dom";
 
 const OpeningScreen = () => {
+  const navigate = useNavigate();
+
   return (
-    <div className="main">
+    <div className="main1">
       <div className="logo">
         <img src={logo} alt="Logo" />
         <div className="name">
-          <p className="Name">VICTUS</p>
+          <p className="Name">𝕍𝕀ℂ𝕋𝕌𝕊</p>
         </div>
         <div className="lines">
         <p className="line">24x7</p>
         <p className="line">Changing lives for the better!</p>
-      </div>
+        </div>
+        <button className="btn" onClick={()=>{navigate("/login")}}>Try for free!</button>
       </div>
     </div>
   );
